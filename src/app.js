b8ef7186086d08689e0961eca0932f897ff83ee9
Ivanzr17 <ivanzafrarendal@gmail.com>
2024-05-26 17:14:03 +0200

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+
+
+//Defino las partes de mis excusas
+
+const quien = ["Luis", "Lia", "Isa"];
+const hizo = ["se comió", "me robó", "se fumó"];
+const que = ["mi proyecto", "mi ordenador", "mis ganas de estudiar"];
+const cuando = ["ayer", "hoy", "la semana pasada"];
+
+//Creo la funcion que randomiza
+
+function random(len) {
+  return Math.floor(Math.random() * len);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hola Mundo");
 };
+
+//Creo mi excusa final concatenado aleatoriamente las excusas definidas anteriormente
+
+const excusa =
+quien[random(quien.length)] +
+" " +
+hizo[random(hizo.length)] +
+" " +
+que[random(que.length)] +
+" " +
+cuando[random(cuando.length)];
+
+// Elijo el elemento del HTML que quiero modificar
+
+document.querySelector("p").innerText = excusa;
